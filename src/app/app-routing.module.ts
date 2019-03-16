46ccd99793ddfc59391b86156589ec564bd094b7
unknown <ignaziocarbonaro@hotmail.com>
2019-03-16 15:50:28 +0100

    Aggiunto lazy loadinig

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule, Router } from '@angular/router';
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { HomeComponent } from "./home/home.component";
 
 const appRoutes: Routes = [
-    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+    // { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+    { path: '', component: HomeComponent},
+    { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'},
     { path: 'shopping-list', component: ShoppingListComponent },
 ];
 @NgModule({
